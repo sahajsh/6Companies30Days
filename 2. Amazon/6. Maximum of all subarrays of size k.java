ca93e157757c2bb90a6629f4ecb17077155427a2
Sahaj Shukla <73556397+sahajsh@users.noreply.github.com>
2022-01-12 23:03:23 +0530

    Update 6. Maximum of all subarrays of size k.java

--- 2. Amazon/6. Maximum of all subarrays of size k.java
+++ 2. Amazon/6. Maximum of all subarrays of size k.java
@@ -22,23 +22,23 @@ class Solution {
 }
 // using deque optimised
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] r = new int[n-k+1];
-        int ri=0;//store index
-        Deque<Integer> q = new ArrayDeque<>();
+    public int[] maxSlidingWindow(int[] nums, int k) {// given an array and sliding window size
+        int n = nums.length;// compute the length of the array
+        int[] r = new int[n-k+1];// declare r stores all our maximums no. of array is n-k+1
+        int ri=0;//store right index
+        Deque<Integer> q = new ArrayDeque<>();// declaredeque
         for(int i =0;i<nums.length;i++){
             //remove numbers out of range k
-            if(!q.isEmpty() && q.peek() == i-k){
-                q.poll();
+            if(!q.isEmpty() && q.peek() == i-k){// q is non empty [peek =front]
+                q.poll();// remove it 
             }
             // remove smaller numbers in k range as they are useless 
             while(!q.isEmpty() && nums[q.peekLast()] <=nums[i]){
-                q.pollLast();
+                q.pollLast(); // remove from last 
             }
-            q.offer(i);
+            q.offer(i);//take current index and pushed it to deque
             if(i>=k-1){
-                r[ri++] =nums[q.peek()];
+                r[ri++] =nums[q.peek()];// we take whatever is in front
             }
         }
         return r;
@@ -46,4 +46,4 @@ class Solution {
         
         
     }
-}
+}//0(n) =time complexity
